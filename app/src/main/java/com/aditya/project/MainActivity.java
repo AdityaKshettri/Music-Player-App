f5566317582e790420401b54e6d4de73b3941f8a
AdityaKshettri <adikshettri1623@gmail.com>
2022-09-01 14:05:13 +0530

    added music player logic

--- app/src/main/java/com/aditya/project/MainActivity.java
+++ app/src/main/java/com/aditya/project/MainActivity.java
@@ -2,13 +2,88 @@ package com.aditya.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button play;
+    private Button pause;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play = findViewById(R.id.play);
+        pause = findViewById(R.id.pause);
+        seekBar = findViewById(R.id.seekBar);
+
+        loadMediaPlayer();
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "Ready to Play!", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+                seekBar.setMax(mediaPlayer.getDuration());
+                seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (fromUser) {
+                            mediaPlayer.seekTo(progress);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+            }
+        });
+
+        // mediaPlayer.start();
+        mediaPlayer.prepareAsync();
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+
+    private void loadMediaPlayer() {
+        // Using music from Local Resource (raw)
+        // mediaPlayer = MediaPlayer.create(this, R.raw.music);
+
+        // Using music from Remote Resource
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("https://paglasongs.com/files/download/id/2094");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
